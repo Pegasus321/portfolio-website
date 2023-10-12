fef0f1c883e98fb0114adc45c65d34da2a2cb980
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-10-12 22:16:35 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/components/about.css";
+import "../styles/components/featured.css";
+import "../styles/components/work.css";
+import "../styles/utils.css";
