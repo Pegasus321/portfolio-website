cb4a1a67dcdd992918ac2557a57398ec55842c8a
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-10-13 17:39:57 +0530

    adding lazy loading

---
+++ notes.md
@@ -0,0 +1,10 @@
+Images that are visible above the fold dont need lazzy loading
+Only images that are below the fold need lazy loading,
+below the fold means anything under the initial viewport
+as in we have to sroll down to see it
+
+there are two ways of lazy loading ...
+
+1. adding loading='lazy' attribute to img tags..
+
+but we use here second way
---
+++ src/utils/mobile-nav.js
@@ -0,0 +1,31 @@
+const mobileNav = () => {
+  const headerBtn = document.querySelector(".header__bars");
+  const mobileNav = document.querySelector(".mobile-nav");
+  const mobileLinks = document.querySelectorAll(".mobile-nav__link");
+
+  // declaring state
+  let isMobileNavOpen = false;
+
+  headerBtn.addEventListener("click", () => {
+    isMobileNavOpen = !isMobileNavOpen;
+    if (isMobileNavOpen) {
+      mobileNav.style.display = "flex";
+
+      //overflowY='hidden' is removing the extra content that was being displayed before after clicking the menu
+      document.body.style.overflowY = "hidden";
+    } else {
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "auto";
+    }
+  });
+
+  mobileLinks.forEach((Link) => {
+    Link.addEventListener("click", () => {
+      isMobileNavOpen = false;
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "auto";
+    });
+  });
+};
+
+export default mobileNav;
